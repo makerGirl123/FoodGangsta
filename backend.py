046495fb981da8242c20b819f438408df500fdd0
SaraJGMU <sjohns99@gmu.edu>
2025-03-30 08:39:43 -0400

    About page update

---
+++ backend.py
@@ -0,0 +1,22 @@
+#manage.py the python file that runs the app. run server run app. (lives in project directory)
+#settings.py project configurations. can have external apps installing. like apis. 
+#apps.py app specific confid file
+#views.py logic for https requests rendering
+#urls.py maps urls to views. Defines routing rules
+#models.py database objects 
+#tests.py automatic app testing... can run in comm line via python test.py
+
+#django 
+#each SQL table represented as object
+
+#stuff from this demo will be on discord
+
+#python3 -m venv .venv
+#source .venv/bin/activate
+#(then will see (.venv) at start of terminal line )
+
+#django-admin startproject tutorial
+
+#---
+
+#idk what I'm doing
